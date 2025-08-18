d958f01976163f8e63148320c80f5865d5288938
Yura_Kashtanov <Kashtanov.yuri@mail.ru>
2025-08-18 21:53:52 +0300

    Спринт 7

--- src/test/java/OrderVerificationTest.java
+++ src/test/java/OrderVerificationTest.java
@@ -1,6 +1,5 @@
 import io.qameta.allure.junit5.AllureJunit5;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
